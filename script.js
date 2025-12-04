57f00e7042f6e6853495f284ccc39bf548826a29
Mahek2710 <2023.mahek.hingorani@ves.ac.in>
2025-12-04 23:56:04 +0530

    Implemented food spawn system and extended snake length when food is eaten

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const blockWidth = 50;
 
 const cols = Math.floor(board.clientWidth / blockWidth);
 const rows = Math.floor(board.clientHeight / blockHeight);
+let intervalId = null;
+let food = {x:Math.floor(Math.random()*rows), y:Math.floor(Math.random()*cols)};
 
 const blocks = [];
 const snake = [{
@@ -24,16 +26,10 @@ for(let row = 0 ; row <rows; row++) {
 
 
 function render(){
-    
-    snake.forEach(segment=>{
-        console.log(blocks[`${segment.x},${segment.y}`].classList.add('fill'));
-    })
-}
-
-
-setInterval(()=>{
 
     let head = null;
+
+    blocks[`${food.x},${food.y}`].classList.add('food');
 
     if(direction === 'left'){
         head = {x:snake[0].x, y:snake[0].y - 1};
@@ -48,6 +44,20 @@ setInterval(()=>{
         head = {x:snake[0].x - 1, y:snake[0].y};
     }
 
+    if(head.x < 0 || head.y < 0 || head.x >= rows || head.y >= cols){
+        alert('Game Over');
+        clearInterval(intervalId);
+    }
+
+
+    if(head.x === food.x && head.y === food.y){
+        blocks[`${food.x},${food.y}`].classList.remove('food');
+        food = {x:Math.floor(Math.random()*rows), y:Math.floor(Math.random()*cols)};
+        blocks[`${food.x},${food.y}`].classList.add('food');
+
+        snake.unshift(head); //head appended in the front after eating food and length increases 
+
+    }
 
     snake.forEach(segment=>{
         blocks[`${segment.x},${segment.y}`].classList.remove('fill');
@@ -56,7 +66,14 @@ setInterval(()=>{
     snake.unshift(head); //shurwaat mein ek element add karega hehe
     snake.pop(); //last element remove karega
 
+    
+    snake.forEach(segment=>{
+        console.log(blocks[`${segment.x},${segment.y}`].classList.add('fill'));
+    })
+}
 
+
+intervalId = setInterval(()=>{
     render();
 },300);
 
